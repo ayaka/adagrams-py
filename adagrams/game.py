97cae81ceb351ed48fbdb420534fcd38a1c0bcab
ayaka <ayakaob@gmail.com>
2021-09-20 20:50:08 -0700

    pass all tests of wave 1 & 2

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,8 +1,57 @@
+import random
+import string
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
 def draw_letters():
-    pass
+    letters = []
+    letters_counter = {}
+    while len(letters) < 10:
+        random_letter = random.choice(string.ascii_uppercase)
+        if letters_counter.get(random_letter) and letters_counter.get(random_letter) >= LETTER_POOL[random_letter]:
+            continue
+        else:
+            letters.append(random_letter)
+            if letters_counter.get(random_letter):
+                letters_counter[random_letter] += 1
+            else:
+                letters_counter[random_letter] = 1
+    return letters
 
 def uses_available_letters(word, letter_bank):
-    pass
+    for letter in word:
+        if word.count(letter) > letter_bank.count(letter):
+            return False
+    return True
+
+        
 
 def score_word(word):
     pass
